63159ec3f7466df1c989b6d8472c6ae2a17f8332
jcy0219 <jicy0219@naver.com>
2023-11-15 16:18:54 +0900

    plus

---
+++ main.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    """두 수의 합을 반환하는 함수"""
+    result = a + b
+    return result
+
+# 함수 호출 예시
+num1 = 5
+num2 = 7
+sum_result = add_numbers(num1, num2)
+
+print(f"{num1}와 {num2}의 합은 {sum_result}입니다.")
