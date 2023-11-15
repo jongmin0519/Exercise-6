eb5687ac05443fe5cb03b63ff708276a2369b83d
jongmin0519 <114043502+jongmin0519@users.noreply.github.com>
2023-11-15 16:50:51 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+
+def add_numbers(a, b):
+    """두 수의 합을 반환하는 함수"""
+    result = a + b
+    return result
+
+# 함수 호출 예시
+num1 = 5
+num2 = 7
+sum_result = add_numbers(num1, num2)
+
+print(f"{num1}와 {num2}의 합은 {sum_result}입니다.")
+=======
 # main.py
 
 def convert_to_uppercase(string):
@@ -17,3 +30,4 @@ if __name__ == "__main__":
     input_string = "Hello, World!"
     uppercase_string = convert_to_uppercase(input_string)
     print(uppercase_string)
+
